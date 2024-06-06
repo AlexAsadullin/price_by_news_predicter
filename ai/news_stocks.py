64ad91f25b1175756794f113fb0b101f4b07704f
Alex Asadullin <151203706+AlexAsadullin@users.noreply.github.com>
2024-06-06 21:47:54 +0300

    model is done, only visualisation and histing remains

--- ai/news_stocks.py
+++ ai/news_stocks.py
@@ -1,11 +1,11 @@
-
+import joblib
 import pandas as pd
 import seaborn as sns
 import re
 import os
 import sys
 
-import pymorphy2
+import pymorphy3
 # import gensim
 
 import nltk
@@ -17,7 +17,6 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from tensorflow.keras import Sequential
 
-
 nltk.download('stopwords')
 '''os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 sns.set_style("darkgrid")'''
@@ -27,7 +26,7 @@ def load_data_csv(name: str):
     parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(f'{name}.csv')))
     news_df = pd.read_csv(f'{parent_dir}/{name}.csv')
 
-    return news_df # pandas dataframe
+    return news_df  # pandas dataframe
 
 
 def format_text(text):
@@ -35,41 +34,45 @@ def format_text(text):
     stopWords = set(stopwords.words('russian'))
 
     reg = re.compile('[^а-яА-Яa-zA-Z0-9 ]')  #
-    text = text.lower().replace("ё", "е").replace("ъ", "ь").replace("й", "и")
+    text = text.lower().replace("ё", "е").replace("ъ", "ь").replace("й", "и").replace("&laquo;", '').replace("&raquo;",
+                                                                                                             '').replace(
+        "&nbsp;", '').replace("&mdash;", '')
     text = reg.sub(' ', text)
     # Лемматизация + stop words
-    morph = pymorphy2.MorphAnalyzer()
+    morph = pymorphy3.MorphAnalyzer()
     text = [morph.parse(word)[0].normal_form for word in text.split() if word not in stopWords]
     text = ' '.join(text)
 
     return text  # is 1 string
 
+
 def text_train_split(df):
     from sklearn.model_selection import train_test_split
-    x_train, x_test, y_train, y_test = train_test_split(df['formatted-text'], df['y'], test_size=.15,
+    x_train, x_test, y_train, y_test = train_test_split(df['formatted-text'], df['growth'], test_size=.15,
                                                         random_state=42)
-    return x_train, x_test, y_train, y_test # strings
+    return x_train, x_test, y_train, y_test  # strings
 
 
 def bow(x_train, x_test):
-    vectorizer = CountVectorizer(ngram_range=1)
+    vectorizer = CountVectorizer(ngram_range=(1, 2))
     x_train_bow = vectorizer.fit_transform(x_train)
     x_test_bow = vectorizer.transform(x_test)
 
-    return x_train_bow, x_test_bow # lists of digits
+    return x_train_bow, x_test_bow  # lists of digits
 
 
 def building_model(x_train_bow, y_train):
+    from sklearn.linear_model import LogisticRegression
     clf = LogisticRegression(random_state=0).fit(x_train_bow, y_train)
-    model = Sequential()
-    # создаем слои нейросети для обучения модели
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    # обучаем модель
-    model.fit(x_train, y_train, epochs=95, batch_size=32)
-    # сохраняем модель
-    model.save('predicter_v1')
-    # возвращаем обученную модель
-    return model
+    joblib.dump(clf, "model.pkl")
+    return clf
+
+
+def test_model(clf, x_train_bow, y_test):
+    from sklearn.metrics import accuracy_score
+    y_predict = clf.predict(x_train_bow)
+    print(x_train_bow, y_test, y_predict)
+    return accuracy_score(y_predict, y_test)
 
 
 if __name__ == '__main__':
@@ -78,5 +81,8 @@ if __name__ == '__main__':
     # creating as;dfkjasd;fkj
     x_train, x_test, y_train, y_test = text_train_split(news_df)
     x_train_bow, x_test_bow = bow(x_train, x_test)
+
+    classificator = building_model(x_train_bow, y_train)
+    # print(test_model(classificator, x_train_bow, y_test))
 
     news_df.to_csv('news_to_learn.csv')
